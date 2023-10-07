27fb0d4f93c242b73039c6ca01d56733c22ceefe
Irina_Ryzhenko <platinumfoxirina@gmail.com>
2023-10-07 19:19:47 +0300

    refactor

--- src/test/java/api/BodyContentTest.java
+++ src/test/java/api/BodyContentTest.java
@@ -1,2 +1,32 @@
-package api;public class BodyContentTest {
+package api;
+
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+
+
+public class BodyContentTest {
+        @Test
+        public void checkBodyContainsParam() throws IOException, ParseException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://api.reverb.com/api/articles");
+        CloseableHttpResponse response = httpClient.execute(httpGet);
+
+        int statusCode = response.getCode();
+        Assertions.assertEquals(200, statusCode);
+
+        String responseBody = EntityUtils.toString(response.getEntity());
+        Assertions.assertTrue(responseBody.contains("\"author_name\":\"Tony Bacon\""),
+                "В теле ответа должен быть экземпляр с параметрами author_name = 'Tony Bacon'");
+            response.close();
+            httpClient.close();
+    }
 }
+
+
--- src/test/java/api/StatusCodeTest.java
+++ src/test/java/api/StatusCodeTest.java
@@ -1,2 +1,27 @@
-package api;public class StatusCodeTest {
+package api;
+
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpResponse;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+
+
+public class StatusCodeTest {
+    @Test
+    public void checkStatusCode() throws IOException {
+
+        CloseableHttpClient httpclient = null;
+        try {
+            httpclient = HttpClients.createDefault();
+            HttpGet httpget = new HttpGet("https://api.reverb.com/api/articles");
+            HttpResponse httpresponse = httpclient.execute(httpget);
+
+            System.out.println(httpresponse.getCode());
+        } finally {
+            httpclient.close();
+            httpclient.close();
+        }
+    }
 }
